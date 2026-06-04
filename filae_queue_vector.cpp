2b9bcd9f775452573581e700124a7e79ec60c3e8
Evelofdex <evelofdex@gmail.com>
2026-06-04 08:08:16 +0700

    switch place

---
+++ filae_queue_vector.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class queue{
+    private:
+    vector<int> data;
+    int dat;
+    int front;
+    int rear;
+
+    public:
+    queue(){ front = rear = -1; }
+
+    void push(){
+
+    }
+    void displayFront(){
+
+    }
+    void displayRead(){
+        
+    }
+    void displayFull(){
+
+    }
+    void pop(){
+
+    }
+};
+
+int main(){
+
+}
